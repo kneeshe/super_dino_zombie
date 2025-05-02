07122050587cbd006ca953af2ebb56996ae174c7
kneeshe <cristian_sousa@live.com>
2025-05-01 23:20:49 -0300

    adições no código e background

--- main.c
+++ main.c
@@ -56,7 +56,7 @@ int main() {
   al_set_new_display_option(ALLEGRO_SAMPLE_BUFFERS, 1, ALLEGRO_SUGGEST);
   al_set_new_display_option(ALLEGRO_SAMPLES, 8, ALLEGRO_SUGGEST);
   al_set_new_bitmap_flags(ALLEGRO_MIN_LINEAR | ALLEGRO_MAG_LINEAR);
-  // al_set_new_display_flags(ALLEGRO_FULLSCREEN_WINDOW);
+  al_set_new_display_flags(ALLEGRO_FULLSCREEN_WINDOW);
   ALLEGRO_DISPLAY *display = al_create_display(WIDTH, HEIGHT);
   must_init(display, "display");
 
@@ -93,8 +93,21 @@ int main() {
 
   must_init(al_init_image_addon(), "image");
   //aqui irão os bitmaps
-  ALLEGRO_BITMAP *dude = al_load_bitmap("./assets/spritesheet.png");
-  must_init(dude, "dude");
+  ALLEGRO_BITMAP *bgImg = al_load_bitmap("./assets/bg/1.png");
+  must_init(bgImg, "1");
+  ALLEGRO_BITMAP *midImg = al_load_bitmap("./assets/bg/2.png");
+  must_init(midImg, "2");
+  ALLEGRO_BITMAP *midImg2 = al_load_bitmap("./assets/bg/3.png");
+  must_init(midImg2, "3");
+  ALLEGRO_BITMAP *midImg3 = al_load_bitmap("./assets/bg/4.png");
+  must_init(midImg3, "4");
+  ALLEGRO_BITMAP *fgImg = al_load_bitmap("./assets/bg/5.png");
+  must_init(fgImg, "5");
+  ALLEGRO_BITMAP *fgImg2 = al_load_bitmap("./assets/bg/6.png");
+  must_init(fgImg2, "6");
+
+  ALLEGRO_BITMAP *hero = al_load_bitmap("./assets/vect.png");
+  must_init(hero, "hero");
 
   al_register_event_source(queue, al_get_keyboard_event_source());
   al_register_event_source(queue, al_get_mouse_event_source());
@@ -112,6 +125,8 @@ int main() {
   //var para o personagem
   float char_x = 100;
   float char_y = 100;
+  float char_x2 = 150;
+  float char_y2 = 150;
   float vel = 5;
 
   //var para o tiro
@@ -131,24 +146,28 @@ int main() {
       case ALLEGRO_EVENT_TIMER:
         if(key[ALLEGRO_KEY_W]) {
           char_y -= vel;
+          char_y2 -= vel;
           if(char_y < 0){
             char_y *= -1;
           }
         }
         if(key[ALLEGRO_KEY_S]){
           char_y += vel;
+          char_y2 += vel;
           if(char_y > HEIGHT){
               char_y -= (char_y - HEIGHT) * 2;
           }
         }
         if(key[ALLEGRO_KEY_A]){
           char_x -= vel;
+          char_x2 -= vel;
           if(char_x < 0) {
             char_x *= -1;
           }
         }
         if(key[ALLEGRO_KEY_D]){
           char_x += vel;
+          char_x2 += vel;
           if(char_x > WIDTH) {
             char_x -= (char_x - WIDTH) * 2;
           }
@@ -180,13 +199,27 @@ int main() {
         break;
     }
 
+    if(event.type == ALLEGRO_EVENT_MOUSE_BUTTON_DOWN) {
+      al_draw_filled_circle(0, 0, 20, al_map_rgb(0, 255, 0));
+
+    }
+
     if(redraw && al_is_event_queue_empty(queue)) {
       al_clear_to_color(al_map_rgb(0, 0, 0));
 
-     al_draw_filled_rectangle(char_x, char_y, char_x + 50, char_y + 50, al_map_rgb(255, 0, 0));
+     al_draw_scaled_bitmap(bgImg, 0, 0, 620, 360, 0, 0, WIDTH, HEIGHT, 0);
+     al_draw_scaled_bitmap(midImg, 0, 0, 620, 360, 0, 0, WIDTH, HEIGHT, 0);
+     al_draw_scaled_bitmap(midImg2, 0, 0, 620, 360, 0, 0, WIDTH, HEIGHT, 0);
+     al_draw_scaled_bitmap(midImg3, 0, 0, 620, 360, 0, 0, WIDTH, HEIGHT, 0);
+     al_draw_scaled_bitmap(fgImg, 0, 0, 620, 360, 0, 0, WIDTH, HEIGHT, 0);
+     al_draw_scaled_bitmap(fgImg2, 0, 0, 620, 360, 0, 0, WIDTH, HEIGHT, 0);
 
-     al_draw_line(shot_x, shot_y, shot_x + 10, shot_y, al_map_rgba(249, 194, 48, 0.15), 4);
-     al_draw_line(shot_x, shot_y, shot_x + 10, shot_y, al_map_rgba(208, 155, 10, 0.6), 2);
+     // al_draw_filled_rectangle(char_x, char_y, char_x2, char_y2, al_map_rgb(255, 0, 0));
+     al_draw_scaled_bitmap(hero, 0, 0, 3000, 3000, char_x, char_y, char_x2, char_y2, 0);
+
+     // al_draw_line(shot_x, shot_y, shot_x + 10, shot_y, al_map_rgba(249, 194, 48, 0.15), 4);
+     // al_draw_line(shot_x, shot_y, shot_x + 10, shot_y, al_map_rgba(208, 155, 10, 0.6), 2);
+
 
       al_flip_display();
     }
@@ -201,10 +234,16 @@ int main() {
   al_destroy_event_queue(queue);
   al_destroy_timer(timer);
   al_destroy_font(font);
-  al_destroy_bitmap(dude);
   al_destroy_sample(scream);
   al_destroy_audio_stream(music);
   al_destroy_audio_stream(music_bg);
+  al_destroy_bitmap(bgImg);
+  al_destroy_bitmap(midImg);
+  al_destroy_bitmap(midImg2);
+  al_destroy_bitmap(midImg3);
+  al_destroy_bitmap(fgImg);
+  al_destroy_bitmap(fgImg2);
+  al_destroy_bitmap(hero);
 
 
   return 0;
